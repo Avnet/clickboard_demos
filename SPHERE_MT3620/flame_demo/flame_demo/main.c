4529c789db3a8ef886164340cbfb938add1e4a53
BeeBoy (James Flynn) <jflynn129@gmail.com>
2019-04-22 18:33:43 -0400

    Jflynn129 dev (#4)
    * clean-up for sphere VC++ project files

--- SPHERE_MT3620/flame_demo/flame_demo/main.c
+++ SPHERE_MT3620/flame_demo/flame_demo/main.c
@@ -1,175 +1,122 @@
-﻿#include <errno.h>
+﻿/**
+* copyright (c) 2018, James Flynn
+* SPDX-License-Identifier: MIT
+*/
+
+#include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "applibs_versions.h"
 #include <applibs/log.h>
 
 #include "mt3620_rdb.h"
 
-// This C application for the MT3620 Reference Development Board (Azure Sphere)
-// outputs a string every second to Visual Studio's Device Output window
-//
-// It uses the API for the following Azure Sphere application libraries:
-// - log (messages shown in Visual Studio's Device Output window during debugging)
-
-static volatile sig_atomic_t terminationRequired = false;
-
-/// <summary>
-///     Signal handler for termination requests. This handler must be async-signal-safe.
-/// </summary>
-static void TerminationHandler(int signalNumber)
-{
-    // Don't use Log_Debug here, as it is not guaranteed to be async-signal-safe.
-    terminationRequired = true;
-}
-
-/// <summary>
-///     Main entry point for this sample.
-/// </summary>
-int main(int argc, char *argv[])
-{
-    Log_Debug("Application starting.\n");
-
-    // Register a SIGTERM handler for termination requests
-    struct sigaction action;
-    memset(&action, 0, sizeof(struct sigaction));
-    action.sa_handler = TerminationHandler;
-    sigaction(SIGTERM, &action, NULL);
-
-    // Main loop
-    const struct timespec sleepTime = {1, 0};
-    while (!terminationRequired) {
-        Log_Debug("Hello world!\n");
-        nanosleep(&sleepTime, NULL);
-    }
-
-    Log_Debug("Application exiting.\n");
-    return 0;
-}
-
-/**
-* copyright (c) 2018, James Flynn
-* SPDX-License-Identifier: MIT
-*/
-
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/un.h>
-#include <sys/syscall.h>
-#include <sys/socket.h>
-
-#include <hwlib/hwlib.h>
-
 #include "flame.h"
 
-static int intPinFD = -1;
-static int irValFD = -1;
+static int intFD = -1;
+static int valFD = -1;
 
 #define IR_THRESHOLD (0.6)
-#define INT_M1       GPIO_PIN_94  //slot #1
-#define INT_M2       GPIO_PIN_7   //slot #2
+#define MIKRO_AN       MT3620_GPIO42  //slot #1 GPIO42_ADC1, Slot #2 GPIO43_ADC2
+#define MIKRO_INT      MT3620_GPIO2   //slot #1 GPIO2_PWM2,  Slot #2GPIO2_PWM2 
 #define GPIO_PIN_XYZ INT_M1
 
-#define delay(x) (usleep(x*1000))   //macro to provide ms pauses
+
+void ms_delay(int msec)
+{
+	struct timespec sleepTime = { 0, 0 };
+	if (msec > 1000)
+		sleepTime.tv_sec = msec / 1000;
+	msec -= sleepTime.tv_sec * 1000;
+	sleepTime.tv_nsec = msec * 1000000;
+
+	nanosleep(&sleepTime, NULL);
+}
 
 void init(void)
 {
-	intPinFD = GPIO_OpenAsInput(MT3620_GPIO67);
-	irValFD = GPIO_OpenAsInput(MT3620_GPIO68);
+	intFD = GPIO_OpenAsInput(MIKRO_INT);
+	valFD = GPIO_OpenAsInput(MIKRO_AN);
 }
 
-int status(void)
+float status(void)
 {
-	///     0 for success, or -1 for failure, in which case errno will be set to the error value.
-/// </returns>
-	int GPIO_GetValue(int gpioFd, GPIO_Value_Type *outValue);
 	GPIO_Value_Type val;
-	if( GPIO_GetValue(irValFD, &val) != 0 )
-	    Log_Debug("ERROR: I2CMaster_SetBusSpeed: errno=%d (%s)\n", errno, strerror(errno));
+	if( GPIO_GetValue(valFD, &val) != 0 ){
+	    Log_Debug("ERROR: status(): errno=%d (%s)\n", errno, strerror(errno));
 		return -1;
 		}
-
-
-	float v;
-	adc_read(irVal, &v);
-	return (v > IR_THRESHOLD) ? 1 : 0;
+	return (val?1.0:0.0);
 }
 
 int intStatus(void)
 {
-	gpio_level_t val;
-	gpio_read(intPin, &val);
+	GPIO_Value_Type val;
+	if (GPIO_GetValue(intFD, &val) != 0) {
+		Log_Debug("ERROR: intStatus(): errno=%d (%s)\n", errno, strerror(errno));
+	return -1;
+    }
 	return val;
 }
 
 void flame_cb(int state)
 {
-	printf("\rInterrupt Line state: %s\n", state ? "HIGH" : "LOW");
+	Log_Debug("\rInterrupt Line state: %s\n", state ? "HIGH (flame detected)" : "LOW (no flame)");
 }
 
 void usage(void)
 {
-	printf(" The 'c_demo' program can be started with several options:\n");
-	printf(" -r X: Set the run-time for 'X' seconds\n");
-	printf(" -?  : Display usage info\n");
+	Log_Debug(" The 'c_demo' program can be started with several options:\n");
+	Log_Debug(" -r X: Set the run-time for 'X' seconds\n");
+	Log_Debug(" -?  : Display usage info\n");
 }
 
 int main(int argc, char *argv[])
 {
 
-	int    m, i, run_time = 30;  //default to 30 second run time
+	int    i, run_time = 30;  //default to 30 second run time
 	FLAME* flameptr;
 
-	while ((i = getopt(argc, argv, "tr:?")) != -1)
+	while ((i = getopt(argc, argv, "r:?")) != -1)
 
 		switch (i) {
 		case 'r':
 			sscanf(optarg, "%x", &run_time);
-			printf(">> run-time set to %d seconds ", run_time);
+			Log_Debug(">> run-time set to %d seconds ", run_time);
 			break;
 		case '?':
 			usage();
 			exit(EXIT_SUCCESS);
 		default:
-			fprintf(stderr, ">> nknown option character `\\x%x'.\n", optopt);
+			Log_Debug(">> nknown option character `\\x%x'.\n", optopt);
 			exit(EXIT_FAILURE);
 		}
 
-	printf("\n\n");
-	printf("     ****\r\n");
-	printf("    **  **     SW reuse using C example\r\n");
-	printf("   **    **    for the Flame Click\r\n");
-	printf("  ** ==== **\r\n");
-	printf("\r\n");
+	Log_Debug("\n\n");
+	Log_Debug("     ****\r\n");
+	Log_Debug("    **  **     SW reuse using C example\r\n");
+	Log_Debug("   **    **    for the Flame Click\r\n");
+	Log_Debug("  ** ==== **\r\n");
+	Log_Debug("\r\n");
 
 	flameptr = open_flamedetect(status, intStatus, init);
 	flame_setcallback(flameptr, flame_cb);
 	run_time *= 4; //convert seconds to 250 msec counts since that is the loop time
-	m = i = 0;
+	i = 0;
 	while (i++ < run_time) {
 		if (flame_status(flameptr))
-			printf("\r->Flame IS detected!\n");
-		else {
-			printf("\r%c", (m == 0) ? '|' : (m == 1) ? '/' : (m == 2) ? '-' : '\\');
-			fflush(stdout);
-			m++;
-			m %= 4;
-		}
+			Log_Debug("\r->Flame detected!\n");
 		flame_intstatus(flameptr);
-		delay(250);
+		ms_delay(250);
 	}
 
-	printf("\r \nDONE...\n");
+	Log_Debug("\r \nDONE...\n");
 	close_flamedetect(flameptr);
 	exit(EXIT_SUCCESS);
 }
