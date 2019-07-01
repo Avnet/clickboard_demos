b0acf0276b4c26d097fee0c09d87304728fa7b40
BeeBoy (James Flynn) <jflynn129@gmail.com>
2019-06-30 21:02:25 -0400

    Update build.sh

--- K64F/click_demo/build.sh
+++ K64F/click_demo/build.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 mbed compile -t gcc_arm -m K64F --source=./ --source=../../clickmodules/
-=======
-mbed compile -t gcc_arm -m K64F --source=./ --source=../../clickmodule/
->>>>>>> origin/master
