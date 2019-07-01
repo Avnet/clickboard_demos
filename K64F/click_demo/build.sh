212606955b460ac5716ea21f5dd7bdb42be5f416
BeeBoy (James Flynn) <jflynn129@gmail.com>
2019-06-30 20:07:50 -0400

    Jflynn129 dev (#20)
    Additing additional support for click boards and updating the submodule.

--- K64F/click_demo/build.sh
+++ K64F/click_demo/build.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
+mbed compile -t gcc_arm -m K64F --source=./ --source=../../clickmodules/
+=======
 mbed compile -t gcc_arm -m K64F --source=./ --source=../../clickmodule/
+>>>>>>> origin/master
